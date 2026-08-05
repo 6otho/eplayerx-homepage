33ec9646eafe4274b4c29b3bd2f43420d8d5ddf2
6otho <124158269+6otho@users.noreply.github.com>
2026-08-06 01:26:12 +0800

    Update index.ts

--- src/crawler/index.ts
+++ src/crawler/index.ts
@@ -476,17 +476,17 @@ const GENRE_TRANSLATIONS: Record<GenreKey, Record<Locale, string>> = {
 };
 
 const GENRE_ITEMS: { id: string; key: GenreKey; imageName: string }[] = [
-  { id: "18", key: "drama", imageName: "Drama-1.webp" },
-  { id: "35", key: "comedy", imageName: "Comedy-1.webp" },
-  { id: "9648,53", key: "thriller", imageName: "Thriller-1.webp" },
-  { id: "28", key: "action", imageName: "Action-1.webp" },
-  { id: "878", key: "scifi", imageName: "Sci-fi-1.webp" },
-  { id: "16", key: "animation", imageName: "Animation-1.webp" },
-  { id: "80", key: "crime", imageName: "Crime-1.webp" },
-  { id: "10749", key: "romance", imageName: "Romance-1.webp" },
-  { id: "10752", key: "war", imageName: "War-1.webp" },
-  { id: "99", key: "documentary", imageName: "Documentary-1.webp" },
-  { id: "10751", key: "kids", imageName: "Kid-1.webp" },
+  { id: "18", key: "drama", imageName: "DRAMA-2.png" },
+  { id: "35", key: "comedy", imageName: "COMEDY-2.png" },
+  { id: "9648,53", key: "thriller", imageName: "THRILLER-2.png" },
+  { id: "28", key: "action", imageName: "ACTION-2.png" },
+  { id: "878", key: "scifi", imageName: "SCI-FI-2.png" },
+  { id: "16", key: "animation", imageName: "ANIMATION-2.png" },
+  { id: "80", key: "crime", imageName: "CRIME-2.png" },
+  { id: "10749", key: "romance", imageName: "ROMANCE-2.png" },
+  { id: "10752", key: "war", imageName: "WAR-2.png" },
+  { id: "99", key: "documentary", imageName: "DOCUMENTARY-2.png" },
+  { id: "10751", key: "kids", imageName: "KID-2.png" },
 ];
 
 function createTmdbListRoute(
