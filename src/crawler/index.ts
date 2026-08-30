26577faf28a147726d6164163471dc4b573b25d5
6otho <124158269+6otho@users.noreply.github.com>
2026-08-30 11:14:32 +0800

    更新 index.ts

--- src/crawler/index.ts
+++ src/crawler/index.ts
@@ -518,17 +518,17 @@ const LANGUAGE_ITEMS = [
 ] as const;
 
 const GENRE_ITEMS: { id: string; key: GenreKey; imageName: string }[] = [
-  { id: "18", key: "drama", imageName: "Drama-1.webp" },
-  { id: "35", key: "comedy", imageName: "Comedy-1.webp" },
-  { id: "9648,53", key: "thriller", imageName: "Thriller-1.webp" },
-  { id: "28", key: "action", imageName: "Action-1.webp" },
-  { id: "878", key: "scifi", imageName: "Sci-fi-1.webp" },
-  { id: "16", key: "animation", imageName: "Animation-1.webp" },
-  { id: "80", key: "crime", imageName: "Crime-1.webp" },
-  { id: "10749", key: "romance", imageName: "Romance-1.webp" },
-  { id: "10752", key: "war", imageName: "War-1.webp" },
-  { id: "99", key: "documentary", imageName: "Documentary-1.webp" },
-  { id: "10751", key: "kids", imageName: "Kid-1.webp" },
+{ id: “18”, key: “drama”, imageName: “DRAMA-2.webp” },
+{ id: “35”, key: “comedy”, imageName: “COMEDY-3.webp” },
+{ id: “9648,53”, key: “thriller”, imageName: “THRILLER-2.webp” },
+{ id: “28”, key: “action”, imageName: “ACTION-2.webp” },
+{ id: “878”, key: “scifi”, imageName: “SCI-FI-2.webp” },
+{ id: “16”, key: “animation”, imageName: “ANIMATION-2.webp” },
+{ id: “80”, key: “crime”, imageName: “CRIME-2.webp” },
+{ id: “10749”, key: “romance”, imageName: “ROMANCE-2.webp” },
+{ id: “10752”, key: “war”, imageName: “WAR-2.webp” },
+{ id: “99”, key: “documentary”, imageName: “DOCUMENTARY-2.webp” },
+{ id: “10751”, key: “kids”, imageName: “KID-2.webp” },
 ];
 
 function createTmdbListRoute(
